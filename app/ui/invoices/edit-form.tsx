55ac07a03b5fcb527f0ffe66cec208c964312962
Wang Xinrong <wangxinrong8@gmail.com>
2024-07-01 17:06:10 +0800

    Add input field warnings

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -19,13 +19,7 @@ export default function EditInvoiceForm({
   customers: CustomerField[];
 }) {
   const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
- 
-//   return (
-//     <form action={updateInvoiceWithId}>
-//       <input type="hidden" name="id" value={invoice.id} />
-//     </form>
-//   )
-// };
+
   return (
     <form action={updateInvoiceWithId}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
